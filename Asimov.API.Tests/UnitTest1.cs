4430087e134ab9abab7a0001f56e06e9e7c635b8
Italo Silva <italo.silva.silvera@outlook.com>
2022-05-03 23:31:33 -0500

    fix test last hopefully

---
+++ Asimov.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Gherkin.Ast;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Asimov.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
